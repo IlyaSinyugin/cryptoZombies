956f1c82555556643f937637738f2bf35c87efe0
Ilya <ilya@sinyugin.ru>
2021-09-13 18:17:34 +0200

    generating random dna and typecasting

--- Lesson1/chapter1.sol
+++ Lesson1/chapter1.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.4.25;
 
 contract ZombieFactory {
 
@@ -11,5 +11,14 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 
 }
